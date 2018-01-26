6abf33db01f1a0378d143203b133dc5b8084493f
Vintizer <vizerkharkiv@gmail.com>
2018-01-27 01:07:45 +0200

    change token

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api')
-const TOKEN = '542127956:AAF6UoO7JY-dAdg9pQ9lwQqILfq7A2Xdt1g'
+const TOKEN = '542127956:AAEaB4KLyperHUt_ugtBEnko_mzlECgLgFg'
 
 const options = {
     polling: {
