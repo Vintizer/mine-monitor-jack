c81c6b803f3f57184c0bc84a6b6e3967102cb8a2
Vintizer <vizerkharkiv@gmail.com>
2018-01-30 21:02:52 +0200

    саня 300

--- support/index.js
+++ support/index.js
@@ -69,7 +69,7 @@ module.exports.calculate = (chatId) => {
                             }
                             break
                         case "sanya":
-                            if (coin === "eth" && hashrate < 370) {
+                            if (coin === "eth" && hashrate < 300) {
                                 resText += "******************************************\n";
                                 bot.sendMessage(
                                     478916730,
