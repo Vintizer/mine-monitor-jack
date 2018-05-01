23b9e58f49ca64e76a58e30378d5177ca8dd430f
Vintizer <vizerkharkiv@gmail.com>
2018-05-01 12:01:42 +0300

    btg x3 troubles

--- support/index.js
+++ support/index.js
@@ -1,114 +1,153 @@
-const https = require('https');
-const bot = require("../bot")
-const config = require('../config.json');
-const { getDataEthermine, getDataNanopool, getDataCoinmine, getDataSuprnova } = require("./getData")
+const https = require("https");
+const bot = require("../bot");
+const config = require("../config.json");
+const {
+  getDataEthermine,
+  getDataNanopool,
+  getDataCoinmine,
+  getDataSuprnova
+} = require("./getData");
 
 const get_json = ({ user, url, pool, coin }) => {
-    return new Promise(function (resolve, reject) {
-        https.get(url, function (res) {
-            let hashrate;
-            let body = '';
-            res.on('data', function (chunk) {
-                body += chunk;
-            });
+  return new Promise(function(resolve, reject) {
+    https.get(url, function(res) {
+      let hashrate;
+      let body = "";
+      res.on("data", function(chunk) {
+        body += chunk;
+      });
 
-            res.on('end', function () {
-                let resBody;
-                try {
-                    resBody = JSON.parse(body);
-                } catch (err) {
-                }
-                if (resBody) {
-                    switch (pool) {
-                        case "ethermine":
-                            hashrate = getDataEthermine(resBody);
-                            break;
-                        case "nanopool":
-                            hashrate = getDataNanopool(resBody);
-                            break;
-                        case "coinmine":
-                            hashrate = getDataCoinmine(resBody, coin);
-                            break;
-                        case "suprnova":
-                            hashrate = getDataSuprnova(resBody, coin);
-                            break;
-                    }
-                    resolve({ hashrate, user, pool, coin });
-                } else {
-                    resolve({ hashrate: "ParseTrouble", user, pool, coin });
-                }
-
-            });
-        });
-    })
+      res.on("end", function() {
+        let resBody;
+        try {
+          resBody = JSON.parse(body);
+        } catch (err) {}
+        if (resBody) {
+          switch (pool) {
+            case "ethermine":
+              hashrate = getDataEthermine(resBody);
+              break;
+            case "nanopool":
+              hashrate = getDataNanopool(resBody);
+              break;
+            case "coinmine":
+              hashrate = getDataCoinmine(resBody, coin);
+              break;
+            case "suprnova":
+              hashrate = getDataSuprnova(resBody, coin);
+              break;
+          }
+          resolve({ hashrate, user, pool, coin });
+        } else {
+          resolve({ hashrate: "ParseTrouble", user, pool, coin });
+        }
+      });
+    });
+  });
 };
 
-module.exports.calculate = (chatId) => {
-    const urlArray = [];
-    for (const user in config) {
-        config[user].forEach(({ pool, coin, url, bound }) => {
-            urlArray.push({
-                pool,
-                coin,
-                url,
-                user
-            })
-        })
-    }
-    Promise.all(urlArray.map(get_json))
-        .then(arr => {
-            let resText = "";
-            if (chatId === 173191632) {
-                arr.forEach(({ user, hashrate, coin }) => {
-                    let userName;
-                    switch (user) {
-                        case "mom":
-                            userName = "Мама - ";
-                            if (hashrate !== false && (coin === "eth" && hashrate < 55 || coin === "btg" && hashrate < 200)) {
-                                resText += "******************************************\n";
-                            }   
-                            break
-                        case "sanya":
-                            if (coin === "eth" && hashrate < 330 || coin === "btg" && hashrate < 550) {
-                                resText += "******************************************\n";
-                                bot.sendMessage(
-                                    478916730,
-                                    "Скорость одной из монет(" + coin + ") упала - " + hashrate + "\r\n"
-                                )
-                            }
-                            userName = "Саня - ";
-                            break
-                        case "me":
-                            if (coin === "eth" && hashrate < 590 || coin === "zen" && hashrate < 500 || coin === "xvg" && hashrate < 15 || coin === "zcl" && hashrate < 250) {
-                                resText += "******************************************\n";
-                            }
-                            userName = "Я - ";
-                            break
-                    }
-                    resText += userName + coin.toUpperCase() + " - " + hashrate + " Mh\n";;
-                })
-            } else {
-                arr.forEach(({ user, hashrate, coin }) => {
-                    let userName;
-                    switch (user) {
-                        case "sanya":
-                            resText += coin.toUpperCase() + " - " + hashrate + " Mh\n";
-                            break
-                    }
-                })
-            }
-            if (resText.indexOf("********") > -1) {
-                bot.sendMessage(
-                    327277912,
-                    resText
-                )
-            }
-            bot.sendMessage(
-                chatId,
-                resText
-            )
-        })
-        .catch(err => { console.log(err); })
-}
+module.exports.calculate = chatId => {
+  let btgMomFalse = 0;
+  let btgMyFalse = 0;
+  let btgSanyaFalse = 0;
+  const urlArray = [];
+  for (const user in config) {
+    config[user].forEach(({ pool, coin, url, bound }) => {
+      urlArray.push({
+        pool,
+        coin,
+        url,
+        user
+      });
+    });
+  }
+  Promise.all(urlArray.map(get_json))
+    .then(arr => {
+      let resText = "";
+      if (chatId === 173191632) {
+        arr.forEach(({ user, hashrate, coin }) => {
+          let userName;
+          switch (user) {
+            case "mom":
+              userName = "Мама - ";
+              if (hashrate !== false) {
+                if (coin === "btg" && hashrate < 300) {
+                  if (btgMomFalse === 2) {
+                    resText += "******************************************\n";
+                  } else {
+                    btgMomFalse++;
+                  }
+                } else {
+                  btgMomFalse = 0;
+                  if (coin === "eth" && hashrate < 55) {
+                    resText += "******************************************\n";
+                  }
+                }
+              }
+              break;
+            case "sanya":
+              if (coin === "btg" && hashrate < 550) {
+                if (btgSanyaFalse === 2) {
+                  resText += "******************************************\n";
+                  bot.sendMessage(
+                    478916730,
+                    "Скорость одной из монет(" +
+                      coin +
+                      ") упала - " +
+                      hashrate +
+                      "\r\n"
+                  );
+                } else {
+                  btgSanyaFalse++;
+                }
+              } else {
+                btgSanyaFalse = 0;
+                if (coin === "eth" && hashrate < 330) {
+                  resText += "******************************************\n";
+                  bot.sendMessage(
+                    478916730,
+                    "Скорость одной из монет(" +
+                      coin +
+                      ") упала - " +
+                      hashrate +
+                      "\r\n"
+                  );
+                }
+              }
+              userName = "Саня - ";
+              break;
+            case "me":
+              if (
+                (coin === "eth" && hashrate < 590) ||
+                (coin === "zen" && hashrate < 500) ||
+                (coin === "xvg" && hashrate < 15) ||
+                (coin === "zcl" && hashrate < 250)
+              ) {
+                resText += "******************************************\n";
+              }
+              userName = "Я - ";
+              break;
+          }
+          resText += userName + coin.toUpperCase() + " - " + hashrate + " Mh\n";
+        });
+      } else {
+        arr.forEach(({ user, hashrate, coin }) => {
+          let userName;
+          switch (user) {
+            case "sanya":
+              resText += coin.toUpperCase() + " - " + hashrate + " Mh\n";
+              break;
+          }
+        });
+      }
+      if (resText.indexOf("********") > -1) {
+        bot.sendMessage(327277912, resText);
+      }
+      bot.sendMessage(chatId, resText);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 module.exports.get_json = get_json;
