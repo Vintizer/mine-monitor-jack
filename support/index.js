a3ecdfa7ce96ffb9627365d36c5869356aa1e927
Vintizer <vizerkharkiv@gmail.com>
2018-05-07 21:05:53 +0300

    mom 48

--- support/index.js
+++ support/index.js
@@ -80,7 +80,7 @@ module.exports.calculate = chatId => {
                   }
                 } else {
                   btgMomFalse = 0;
-                  if (coin === "eth" && hashrate < 55) {
+                  if (coin === "eth" && hashrate < 48) {
                     resText += "******************************************\n";
                   }
                 }
